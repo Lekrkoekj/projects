6938e893a0b9500c2ac47683813b4f8915c5c713
Lekrkoekj <50798931+Lekrkoekj@users.noreply.github.com>
2020-06-29 22:55:19 +0200

    index fix

--- lkc-changelog.js
+++ lkc-changelog.js
@@ -1,5 +1,5 @@
 document.getElementById("lkc-changelog").onload = LKCChangelog();
 
 function LKCChangelog() {
-    document.getElementById("lkd-changelog").innerHTML = '<p>Note: This game is currently only available in Dutch! Support for English will be added soon.</p><h1 style="text-align: center;">New in Version 1.2:</h1><p>Brand new UI added!</p><p>Added Quests!</p><p>Added KoekjCoins!</p><p>Powerups added! There are currently 2 powerups, more will be added later.</p><p>Bugfixes.</p><h1 style="text-align: center;">v1.2.1:</h1><p>More bugfixes.</p>'
+    document.getElementById("lkc-changelog").innerHTML = '<p>Note: This game is currently only available in Dutch! Support for English will be added soon.</p><h1 style="text-align: center;">New in Version 1.2:</h1><p>Brand new UI added!</p><p>Added Quests!</p><p>Added KoekjCoins!</p><p>Powerups added! There are currently 2 powerups, more will be added later.</p><p>Bugfixes.</p><h1 style="text-align: center;">v1.2.1:</h1><p>More bugfixes.</p>'
 }
