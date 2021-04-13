a9cb921deb5ee9775a5b2ee8cbf4720609b6eb54
Lekrkoekj <50798931+Lekrkoekj@users.noreply.github.com>
2021-04-13 15:48:00 +0200

    Domain fixes

--- scripts/create-short-url.js
+++ scripts/create-short-url.js
@@ -16,7 +16,7 @@ var customPath = document.getElementById("custom-path").value;
         document.getElementById("error-text").innerHTML = "URLs can not have spaces in them.";
     }
     else {
-        var data = "{\"allowDuplicates\":false,\"domain\":\"l.lekr.ml\",\"originalURL\":\"" + originalUrl + "\",\"path\":\"" +  customPath + "\"}";
+        var data = "{\"allowDuplicates\":false,\"domain\":\"l.lekr.xyz\",\"originalURL\":\"" + originalUrl + "\",\"path\":\"" +  customPath + "\"}";
 
         var xhr = new XMLHttpRequest();
         xhr.addEventListener("readystatechange", function () {
